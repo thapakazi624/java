b66d7605cf73336bba05a9fa21440d28d26d7ec5
Ashok <ashokthapa2052@gmail.com>
2019-07-04 02:05:56 +0545

    update interface

--- interface/interface2.java
+++ interface/interface2.java
@@ -1,8 +1,9 @@
+//creating class that implements from two interfaces
 class interface2 implements i1,i2 {
 	public void method1()
 	{
-		System.out.println(i1.i);
-		System.out.println(i2.i);
+		System.out.println(i1.i);//reads the value of i from interface i1
+		System.out.println(i2.i);//reads the value of i from interface i2
 	}
 	public static void main(String[] args) {
 		interface2 obj2 = new interface2();
