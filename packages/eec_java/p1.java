254aa5d1504bc14612dcc5b3c6a3cbb3199ef3ca
Ashok <ashokthapa2052@gmail.com>
2019-07-05 17:53:51 +0545

    packages

---
+++ packages/eec_java/p1.java
@@ -0,0 +1,13 @@
+package eec_java;
+public class p1
+{
+	int a;
+	public int b;
+	private int c;
+	protected int d;
+	public void method1(){
+		System.out.println("sum is: "+(a+b+c+d));
+	
+	}
+	
+}
---
+++ packages/p1test.java
@@ -0,0 +1,9 @@
+import eec_java.*;
+class p1test
+{
+	public static void main(String[] args) {
+		p1 obj = new p1();
+		obj.method1();
+	}
+
+}
