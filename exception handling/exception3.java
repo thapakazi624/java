dec8749284ae9809778d681586bb077b9f5f4fa8
Ashok <ashokthapa2052@gmail.com>
2019-07-06 15:07:41 +0545

    exception handling

---
+++ exception handling/exception3.java
@@ -0,0 +1,45 @@
+//uses of the throws statement
+class myexception extends Exception
+{
+	myexception(String s)
+	{
+		super(s);
+	}
+} 
+class exception3
+{
+	static void validage(int age) throws myexception
+	{
+		if(age<18)
+		{
+			//throws an exception if the above question doesnt satisfy
+			throw new myexception("you are not valid to vote");
+		}
+		else
+		{
+			System.out.println("welcome to vote");
+		}
+
+	}
+	public static void main(String args[])
+	 {
+	 	//checking for the input validations and catching the exception thrown above
+		try
+		{
+			validage(Integer.parseInt(args[0]));
+		}
+		catch(myexception my)
+		{
+			
+
+			System.out.println(my.getMessage());
+		}
+		
+			finally
+			{
+				System.out.println("execution complete");
+			}
+		
+	}
+	
+}
